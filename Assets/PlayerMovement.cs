3c7e95f67047b4afa6922c487f73e7e3e6c7903c
Bhesaj <bhesaj444@gmail.com>
2019-01-08 09:01:42 +0530

    First commit
    - 3 levels

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 10f;
+    public Joystick myJs;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        float x = myJs.Horizontal;
+        float y = myJs.Vertical;
+        rb.velocity = new Vector2(x * speed,y * speed );
+           
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Goal")
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
